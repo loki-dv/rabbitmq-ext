b2647ab202fee3131a1fff5577ccc6868e010ecf
Никита Бабич <nikita.babich@flant.ru>
2016-02-25 23:52:02 +0300

    added items

--- rabbit-ext-stat2.sh
+++ rabbit-ext-stat2.sh
@@ -13,6 +13,9 @@ if [ "$dif" -gt "55" ]; then
   curl -i -u zabbix:zabbix http://localhost:15672/api/queues | sed -n '7,${p;}' | jq '.[] | .messages' > /tmp/rabbit-ext-stat-messages
   curl -i -u zabbix:zabbix http://localhost:15672/api/queues | sed -n '7,${p;}' | jq '.[] | .memory' > /tmp/rabbit-ext-stat-memory
   curl -i -u zabbix:zabbix http://localhost:15672/api/queues | sed -n '7,${p;}' | jq '.[] | .message_stats.deliver' > /tmp/rabbit-ext-stat-deliver
+  curl -i -u zabbix:zabbix http://localhost:15672/api/queues | sed -n '7,${p;}' | jq '.[] | .slave_nodes | length' > /tmp/rabbit-ext-stat-slave-nodes
+  curl -i -u zabbix:zabbix http://localhost:15672/api/queues | sed -n '7,${p;}' | jq '.[] | .synchronised_slave_nodes | length' > /tmp/rabbit-ext-stat-sync-slave-nodes
+  curl -i -u zabbix:zabbix http://localhost:15672/api/queues | sed -n '7,${p;}' | jq '.[] | .recoverable_slaves | length' > /tmp/rabbit-ext-stat-rec-slave-nodes
   date +%s > /tmp/rabbit-ext-stat-timestamp
 fi
 
@@ -25,11 +28,20 @@ if [ "$1" -eq "0" ]; then
   # Generate JSON
   printf "{\n"
   printf "\t\"data\":[\n\n"
-  for line in `cat /tmp/rabbit-ext-stat-ids`; do
+  lines=`cat /tmp/rabbit-ext-stat-names | wc -l`
+  for i in `seq 1 $lines`; do
+    line=`sed -n -e "$i"p /tmp/rabbit-ext-stat-names`
     printf "\t{\n"
-    printf "\t\t\"{#QUEUEID}\":\"$line\"\n"
-    printf "\t}\n"
+    printf "\t\t\"{#QUEUEID}\":\"$i\",\n"
+    printf "\t\t\"{#QUEUENAME}\":\"$line\"\n"
+    if [ "$i" -ne "$lines" ]; then
+      printf "\t},\n"
+    else
+      printf "\t}\n"
+    fi
   done
+
+
   printf "\n\t]\n"
   printf "}\n"
   exit 0
@@ -57,6 +69,21 @@ case "$2" in
     ;;
   id)
     sed -n -e "$1p" /tmp/rabbit-ext-stat-ids
+    ;;
+  alive_telnet)
+    if echo q | telnet -e q localhost 15672| grep -oq Connected &> /dev/null
+      then echo 1
+      else echo 0
+    fi;
+    ;;
+  slave)
+    sed -n -e "$1p" /tmp/rabbit-ext-stat-slave-nodes
+    ;;
+  sync-slave)
+    sed -n -e "$1p" /tmp/rabbit-ext-stat-sync-slave-nodes
+    ;;
+  rec-slave)
+    sed -n -e "$1p" /tmp/rabbit-ext-stat-rec-slave-nodes
     ;;
   *)
     echo "Usage: rabbit-ext-stat.sh id metric. Metric missing or not valid."
